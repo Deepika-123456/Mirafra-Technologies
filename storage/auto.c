d91176ff83289f4a5dc3c1dd9cd1d304dace21a4
KMMT01ESD02 <deepikapinninti29@gmail.com>
2023-03-24 11:43:34 +0530

    automatic

---
+++ storage/auto.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+auto int a=2;
+int main()
+{
+ int c,b=3;
+c=a+b;
+printf("%d",c);
+}
+
---
+++ storage/auto_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ int a=2;
+int main()
+{
+auto int c,b=3;
+
+c=a+b;
+printf("%d",c);
+}
+
